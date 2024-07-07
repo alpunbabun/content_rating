cdbcf7033a52eeee164f24d21e07680efaac45a4
alfonz dela torre <alfonz.durandelatorre@gmail.com>
2024-07-07 11:49:41 -0400

    event handlers

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -3,11 +3,22 @@ import React, { Component } from 'react';
 import './ContentRating.css';
 
 class ContentRating extends Component {
-  constructor() {
+   constructor() {
     super();
     this.state = {
-        likes: 0,
-        dislikes: 0
+      likes: 0,
+      dislikes: 0,
+      handleLike:() => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1
+        }));
+      },
+
+      handleDislike:() => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1
+        }));
+      }
     };
   }
   render() {
@@ -18,10 +29,10 @@ class ContentRating extends Component {
                 Text Content Rating
             </p>
             <div className='rating-buttons'>
-                <button className="like-button">
+                <button className="like-button" onClick={this.state.handleLike}>
                     Like ({this.state.likes})
                 </button>
-                <button className="dislike-button">
+                <button className="dislike-button" onClick={this.state.handleDislike}>
                     Dislike ({this.state.dislikes})
                 </button>
             </div>
